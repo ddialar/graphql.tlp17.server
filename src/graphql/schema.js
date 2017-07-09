f1048a8c57c88d992a51d5a7da040051b687f344
ddialar <ddialar@gmail.com>
2017-07-09 11:45:14 +0100

    feat(graphql): Include the 'schema' file template.

---
+++ src/graphql/schema.js
@@ -0,0 +1,9 @@
+/*
+En este archivo implementaremos el "schema" que utilizará GraphQL para poder gestionar los datos de nuestra aplicación.
+*/
+
+import axios from 'axios';
+import { GraphQLSchema } from 'graphql';
+
+export default new GraphQLSchema({});
+
