80b7e57fa51da12b8fc72721e0b67ec38f0711f2
ddialar <ddialar@gmail.com>
2017-07-12 22:38:03 +0100

    chore(step-4): Define the base file contents.

--- src/graphql/models/director.type.js
+++ src/graphql/models/director.type.js
@@ -4,12 +4,20 @@ import {
     GraphQLString
 } from 'graphql';
 
+// En esta fase del proyecto, deberás importar el servicio MoviesDirectorsService para,
+// de este modo, poder acceder al método 'getMoviesDataByDirectorId' que nos proporcionará
+// la información que solicitamos.
+
 var DirectorType = new GraphQLObjectType({
     name: 'Director',
     fields: () => ({
         id: { type: GraphQLInt },
-        name: { type: GraphQLString }
+        name: { type: GraphQLString },
+        tarea: 'Añadir el campo movies para poder obtener las películas relacionadas con este director'
     })
 });
 
 export default DirectorType;
+
+// Además, también tendrás que importar el objeto tipo MovieType para poder devolver la
+// información solicitada en el formato correcto.
